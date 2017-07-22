7f67cb18d0ddc23d908d013e2c58870d8a874774
97ece <30343837+97ece@users.noreply.github.com>
2017-07-22 16:59:56 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter a number");
+scanf("%d",&n);
+if(a>0)
+printf("\n number is positive");
+elseif(a<0)
+ printf("\n number is negative");
+ else
+ printf("\n number is zero");
+ getch();
+ }
